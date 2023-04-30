0b3ca2141f4fce0f374e560cca3e18ad450f915e
Niklas Marion <niklas.marion@gmx.de>
2023-04-30 11:22:08 +0200

    add static live result redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  redirects: async () => {
+    return [
+      {
+        source: '/ergebnisse/live',
+        destination: 'https://www.leichtathletik.de/termine/wettkampf-kalender/veranstaltung/detail/58-Internationales-Pfingstsportfest-Rehlingen-23V14000008205101',
+        permanent: false,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
