b6e22018149614d07114e137e82d52ec43792ac2
Niklas Marion <niklas.marion@gmx.de>
2023-05-03 23:12:53 +0200

    add timetable

--- src/app/zeitplan/page.tsx
+++ src/app/zeitplan/page.tsx
@@ -5,16 +5,255 @@ export const metadata = {
   title: "Zeitplan",
   openGraph: {
     title: "Zeitplan",
-  }
+  },
 };
 export default function Zeitplan() {
   return (
     <ContentContainer>
       <Title>Zeitplan</Title>
       <article className="prose prose-xl max-w-none prose-table:tracking-wide">
-        <p className="text-center">
-          Der Zeitplan wird in Kürze veröffentlicht.
-        </p>
+        <h2>VSE-Nachwuchs Vorprogramm</h2>
+        <div className="overflow-x-auto">
+          <table>
+            <thead>
+              <tr>
+                <th>Uhrzeit</th>
+                <th>Lauf</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>15.00</td>
+                <td>800 m W14</td>
+              </tr>
+              <tr>
+                <td>15.08</td>
+                <td>800 m W15</td>
+              </tr>
+              <tr>
+                <td>15.16</td>
+                <td>800 m M14 / M15</td>
+              </tr>
+              <tr>
+                <td>15.24</td>
+                <td>800m W U18/ U20</td>
+              </tr>
+              <tr>
+                <td>15.34</td>
+                <td>800 m M U18/U20</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <h2>Hauptprogramm - Offizielle Eröffnung 15:50</h2>
+        <div className="overflow-x-auto">
+          <table>
+            <thead>
+              <tr>
+                <th>Uhrzeit</th>
+                <th>Lauf</th>
+                <th>Sprung</th>
+                <th>Wurf</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>16.03</td>
+                <td>Pendelstaffel</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>16.09</td>
+                <td></td>
+                <td>Hochsprung Männer</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>16.12</td>
+                <td></td>
+                <td></td>
+                <td>Speerwurf Frauen</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>16.18</td>
+                <td>4 x 400m Mixed U12/U14</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>16.30</td>
+                <td>100m VL Männer</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>16.45</td>
+                <td></td>
+                <td>Weitsprung Para Männer/Frauen</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>16.50</td>
+                <td>400m Hürden Zeitläufe</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>16.55</td>
+                <td></td>
+                <td>Stabhochsprung Männer</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>17.00</td>
+                <td>400m Hürden Zeitläufe</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>17.12</td>
+                <td>800m Frauen Zeitläufe</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>17.20</td>
+                <td>800m Frauen Zeitläufe</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>17.27</td>
+                <td></td>
+                <td></td>
+                <td>Kugel Frauen</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>17.33</td>
+                <td>100m VL Frauen</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>17.40</td>
+                <td>100m Zeitendläufe Männer</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>17.53</td>
+                <td>3000m Hindernis Männer</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>17.58</td>
+                <td></td>
+                <td></td>
+                <td>Speerwurf Männer</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>18.00</td>
+                <td></td>
+                <td>Hochsprung Frauen</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>18.08</td>
+                <td>1500m Männer</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>18.20</td>
+                <td>400m Frauen Zeitläufe</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>18.25</td>
+                <td>400m Frauen Zeitläufe</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>18.37</td>
+                <td>800m Männer Zeitläufe</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>18.45</td>
+                <td>800m Männer Zeitläufe</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>18.53</td>
+                <td>100m Frauen Finale</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>19.03</td>
+                <td>1500m Frauen</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </article>
     </ContentContainer>
   );
