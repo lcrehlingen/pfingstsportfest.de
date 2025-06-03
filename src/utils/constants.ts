993b792ce263f406aa35bb692520e7b384c0fd39
Niklas Marion <niklas.marion@gmx.de>
2025-06-03 22:17:42 +0200

    update stream link

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,7 @@ const EVENT_DATE = new Date(EDITION_DATE_FROM + 'T00:00:00.000Z');
 const TOTAL_PRIZEMONEY = 78000;
 const LIVE_RESULTS = "https://red.laportal.net/Competitions/Details/15155";
 const INTERNATIONAL_LIVESTREAM = "https://www.youtube.com/watch?v=nts7TqhqbOM";
-const NATIONAL_LIVESTREAM = "https://www.youtube.com/watch?v=0vQL8Yz00h0";
+const NATIONAL_LIVESTREAM = "https://www.youtube.com/@saarlaendischerrundfunk";
 const PHOTOFINISH_LINK = "https://photofinish.lcrehlingen.de/pfingsten2025";
 
 const RESULT_LINKS = [
