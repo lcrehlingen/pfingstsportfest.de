a95d040056c0b2a9f48cc9df536e6a800f3585aa
Niklas Marion <niklas.marion@gmx.de>
2026-06-28 16:42:21 +0200

    Revert "add press mentions page"
    This reverts commit 70b310dbf2fdc4a2f1c767e2b760a192537bb9ba.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,7 +15,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     "/ergebnisse",
     "/news",
     "/presse",
-    "/pressespiegel",
     "/ranking",
     "/rekorde",
     "/zeitplan",
